1f315a153fb175100a8d9b7df5de05a44cd5ce50
FhatimaReyes <fhareal10@gmail.com>
2023-04-18 02:14:34 -0600

    add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import imagenRM from "./img/rick-morty.png"
+import credencial from "./img/credentials.png"
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
+
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  };
+
+  console.log(characters)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <h1 className="title"> Rick and Morty </h1>
+        <h2 className="title-2"> Fhatima Reyes Alejandre - S20006773</h2>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imagenRM} alt="Rick & Morty" className="img-home"></img>
+            <button onClick={reqApi} className="btn-search">Buscar Personaje</button>
+            <img src={credencial} alt="center" width={190} align="left"></img>
+          </>
+        )}
       </header>
+      
     </div>
+    
   );
 }
 
